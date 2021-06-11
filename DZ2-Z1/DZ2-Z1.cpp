9a7cbc02a526289caae34006cbbfa7199df4a165
Evgeniy Kolupaev <E.A.Kolupaev@gmail.com>
2021-06-11 18:23:30 +0300

    Первые варианты программ

---
+++ DZ2-Z1/DZ2-Z1.cpp
@@ -0,0 +1,32 @@
+﻿// Решить уравнение.
+
+#include <stdio.h>
+#include <math.h>
+
+const double M_PI = 3.14;
+
+
+int main()
+
+{
+	double x;                                               // вводимое значение
+	double y_rad;                                           // выводимое значение в радианах
+	double y_grad;                                          // выводимое значение в градусах
+	double znach_cos;
+	double znach_sin;
+	double znach_log;
+	double znach_e;
+	printf("Plese enter x:");
+	scanf("%lf", &x);
+	znach_cos = (2 * cos(x - (M_PI / 6))) + sqrt(2);              // вычисляем значение делимого
+	znach_log = 1 / (2 * log(x));                                 // вычисляем значение делителя с ln
+	znach_sin = (1 - cos(2 * pow(x, 2))) / 2;                     // вычисляем значение делителя с sin
+	znach_e = exp(3 * x);                                         // вычисляем значение e
+	y_rad = (znach_cos / (znach_log + znach_sin)) * znach_e;      // выводимое значение в радианах
+	y_grad = y_rad * M_PI / 180;                                  // ввыводимое значение в градусах
+
+	printf("In radians %f\n", y_rad);
+	printf("In degrees %f\n", y_grad);
+
+	return 0;
+}
---
+++ DZ2-Z2/DZ2-Z2.cpp
@@ -0,0 +1,39 @@
+﻿// Расчитать площади круга, треугольника, квадрата и сравнить эти значение между собой.вывести наибольшее значение. 
+#include <stdio.h>
+#include <math.h>
+
+const double M_PI = 3.14;
+
+
+int main()
+{
+    double R_circle;     // Радиус круга
+    double a_triangle;   // Сторона треугольника
+    double d_square;     // Сторона квадрата
+    double S_circle;     // Площадь круга
+    double S_triangle;   // Площадь треугольника
+    double S_square;     // Площадь квадрата
+
+  //Получение значения радиуса круга
+    printf("Plese enter radius circle:");
+    scanf("%lf/n", &R_circle);
+
+    //Получение значения стороны треугольника  
+    printf("Plese enter side triangle:");
+    scanf("%lf/n", &a_triangle);
+
+    //Получение значения стороны квадрата
+    printf("Plese enter side square:");
+    scanf("%lf/n", &d_square);
+
+    S_circle = M_PI * pow(R_circle, 2);             // Расчет площади круга
+    S_triangle = (pow(a_triangle, 2) * sqrt(3)) / 4;    // Расчет площади треугольника   
+    S_square = d_square * d_square;                // Расчет площали квадрата
+
+    //Вывод результата
+    printf("Largest area - ");
+    S_circle >= S_triangle ? S_circle >= S_square ? printf("circle= %f", S_circle) : printf("square = %f", S_square) : S_triangle >= S_square ? printf("triangle= %f", S_triangle) : printf("square = %f", S_square);
+
+
+    return 0;
+}
